98ee95d5ac373e84a7159934038ec95e9db65470
Nikhil-sha <nikhil.sharma.main@gmail.com>
2024-08-06 16:55:08 +0530

    indentation and folder adjustment

--- scripts/main.js
+++ scripts/main.js
@@ -6,41 +6,41 @@ const deskbarMenu = document.getElementById("deskbar-menu");
 
 // Function to load the HTML file and set its content as innerHTML of the div
 function loadHTML(iPageUrl, iPagejs) {
-    let pageUrl = 'https://raw.githubusercontent.com/Nikhil-sha/Windows-clone-in-html/main' + iPageUrl;
-    let pagejs = 'https://raw.githubusercontent.com/Nikhil-sha/Windows-clone-in-html/main' + iPagejs;
-    fetch(pageUrl)
-        .then(response => response.text())
-        .then(data => {
-            homeWindow.innerHTML = data;
-            tabNavMenu.style.display = (iPageUrl === "/pages/application-drawer.html") ? "none" : "block";
-            toolbar.style.display = (iPageUrl === "/pages/application-drawer.html") ? "none" : "block";
-            
-            if (pagejs) {
-                // Remove existing script if present
-                const existingScript = document.getElementById("page-script");
-                if (existingScript) {
-                    existingScript.parentNode.removeChild(existingScript);
-                }
-                
-                // Create a new script element
-                const script = document.createElement('script');
-                script.src = pagejs;
-                script.async = true; // Load the script asynchronously
-                script.id = "page-script";
-                document.head.appendChild(script);
-            }
-        })
-        .catch(error => console.error('Error fetching the HTML file / showing toolbar:', error));
+	let pageUrl = 'https://raw.githubusercontent.com/Nikhil-sha/Windows-clone-in-html/main' + iPageUrl;
+	let pagejs = 'https://raw.githubusercontent.com/Nikhil-sha/Windows-clone-in-html/main' + iPagejs;
+	fetch(pageUrl)
+		.then(response => response.text())
+		.then(data => {
+			homeWindow.innerHTML = data;
+			tabNavMenu.style.display = (iPageUrl === "/pages/application-drawer.html") ? "none" : "block";
+			toolbar.style.display = (iPageUrl === "/pages/application-drawer.html") ? "none" : "block";
+
+			if (pagejs) {
+				// Remove existing script if present
+				const existingScript = document.getElementById("page-script");
+				if (existingScript) {
+					existingScript.parentNode.removeChild(existingScript);
+				}
+
+				// Create a new script element
+				const script = document.createElement('script');
+				script.src = pagejs;
+				script.async = true; // Load the script asynchronously
+				script.id = "page-script";
+				document.head.appendChild(script);
+			}
+		})
+		.catch(error => console.error('Error fetching the HTML file / showing toolbar:', error));
 }
 
 // Toggle function for the start button
 function startButtonAction() {
-    document.querySelector(".start-window").classList.toggle('start-window-opened');
+	document.querySelector(".start-window").classList.toggle('start-window-opened');
 }
 
 // Toggle function for the deskbar menu
 function deskbarMenuAction() {
-    document.querySelector(".deskbar-menu-dialog-box").classList.toggle('deskbar-menu-dialog-box-opened');
+	document.querySelector(".deskbar-menu-dialog-box").classList.toggle('deskbar-menu-dialog-box-opened');
 }
 
 // Add event listeners to buttons
